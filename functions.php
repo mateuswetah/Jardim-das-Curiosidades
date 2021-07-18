ebaa83d9318df0442538b746b889c10e60b9af3a
mateuswetah <mateus.m.luna@gmail.com>
2021-07-18 20:15:28 -0300

    Adds Poppings font family.

--- functions.php
+++ functions.php
@@ -18,5 +18,8 @@ function jardim_das_curiosidades_enqueue_styles() {
 
 	// Load the stylesheet
 	wp_enqueue_style( 'jardim-das-curiosidades-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), JARDIM_DAS_CURIOSIDADES_VERSION );
+
+	// Loads Google Font Poppins
+	wp_enqueue_style( 'jardim-das-curiosidades-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap', false ); 
 }
 add_action( 'wp_enqueue_scripts', 'jardim_das_curiosidades_enqueue_styles' );
