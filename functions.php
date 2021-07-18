8c3b0a580c124323b826e39356f940d809999a59
mateuswetah <mateus.m.luna@gmail.com>
2021-07-18 19:18:43 -0300

    Begins styling.

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if (! defined('WP_DEBUG')) {
 }
 
 /** Child Theme version */
-const JARDIM_DAS_CURIOSIDADES_VERSION = 0.0.1;
+const JARDIM_DAS_CURIOSIDADES_VERSION = 0.0.2;
 
 /**
  * Load the parent style.css file
