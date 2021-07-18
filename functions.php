baec3acb74e573ce1291866e9d3a6cc370423d1c
mateuswetah <mateus.m.luna@gmail.com>
2021-07-18 20:37:32 -0300

    Experiments loading parent style as previously.

--- functions.php
+++ functions.php
@@ -13,9 +13,11 @@ const JARDIM_DAS_CURIOSIDADES_VERSION = '0.0.2';
  * @link http://codex.wordpress.org/Child_Themes
  */
 function jardim_das_curiosidades_enqueue_styles() {
+	// Enqueue parent styles
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', [], JARDIM_DAS_CURIOSIDADES_VERSION );
 
 	// Load the stylesheet
-	wp_enqueue_style( 'jardim-das-curiosidades-style', get_stylesheet_directory_uri() . '/style.css', array('twentyfifteen-style-css'), JARDIM_DAS_CURIOSIDADES_VERSION );
+	wp_enqueue_style( 'jardim-das-curiosidades-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), JARDIM_DAS_CURIOSIDADES_VERSION );
 
 	// Loads Google Font Poppins
 	wp_enqueue_style( 'jardim-das-curiosidades-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap', false ); 
