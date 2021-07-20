8b5ba652234f88bdc83f026d2e5572b8de50f3d8
mateuswetah <mateus.m.luna@gmail.com>
2021-07-19 21:47:58 -0300

    Creates theme.json to set gutenberg variables.

--- functions.php
+++ functions.php
@@ -23,3 +23,11 @@ function jardim_das_curiosidades_enqueue_styles() {
 	wp_enqueue_style( 'jardim-das-curiosidades-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap', false ); 
 }
 add_action( 'wp_enqueue_scripts', 'jardim_das_curiosidades_enqueue_styles' );
+
+/**
+ * Loads style related to the editor side
+ */
+function jardim_das_curiosidades_add_editor_style() {
+	add_editor_style( 'style-editor.css' );
+}
+add_action( 'init', 'jardim_das_curiosidades_add_editor_style' );
