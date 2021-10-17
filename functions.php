143a4924782ba834fac11d3996667fd2bd76e44d
mateuswetah <mateus.m.luna@gmail.com>
2021-10-17 17:35:46 -0300

    Updates to fit new brigther design.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function jardim_das_curiosidades_enqueue_styles() {
 	wp_enqueue_style( 'jardim-das-curiosidades-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), JARDIM_DAS_CURIOSIDADES_VERSION );
 
 	// Loads Google Font Poppins
-	wp_enqueue_style( 'jardim-das-curiosidades-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap', false ); 
+	wp_enqueue_style( 'jardim-das-curiosidades-fonts', 'https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,400;0,700;1,400;1,700&display=swap', false ); 
 }
 add_action( 'wp_enqueue_scripts', 'jardim_das_curiosidades_enqueue_styles' );
 
